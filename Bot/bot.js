2da6a9d55a905775853dde977c420c0606113b06
hieu <duchieu171299@gmail.com>
2024-04-16 18:10:54 +0700

    init

--- Bot/bot.js
+++ Bot/bot.js
@@ -3,24 +3,32 @@ const  { Keyboard } = require('telegram-keyboard')
 const TOKEN = "7011088128:AAGUjF7Wj7EOIdaHi--PXhq48rT9rJPZIos";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://tele-food-app.vercel.app/"
-bot.start((ctx) =>
-  ctx.reply("Welcome", {
+const web_link = "https://tele-food-clone.vercel.app/"
+// bot.start((ctx) =>
+//   ctx.reply("Welcome", {
+//     reply_markup: {
+//       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//     },
+//   })
+// );
+
+// bot.hears('text', async (ctx) => {
+//   const keyboard = Keyboard.make([
+//     ['Button 1', 'Button 2'], // First row
+//     ['Button 3', 'Button 4'], // Second row
+//   ])
+  
+//   await ctx.reply('Simple built-in keyboard', keyboard.reply())
+//   // await ctx.reply('choose' , keyboard.reply())
+//   // await ctx.reply('Simple inline keyboard', keyboard.inline())
+// })
+
+bot.hears('clone', async (ctx) => {
+  await ctx.reply("Welcome App Clone", {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
   })
-);
-
-bot.hears('text', async (ctx) => {
-  const keyboard = Keyboard.make([
-    ['Button 1', 'Button 2'], // First row
-    ['Button 3', 'Button 4'], // Second row
-  ])
-  
-  await ctx.reply('Simple built-in keyboard', keyboard.reply())
-  // await ctx.reply('choose' , keyboard.reply())
-  // await ctx.reply('Simple inline keyboard', keyboard.inline())
 })
 
 bot.hears('Search', ctx => ctx.reply('Yay!'))
